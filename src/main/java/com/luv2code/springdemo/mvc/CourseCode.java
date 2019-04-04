fbb79f6058b7b5d264dc8c8d554a7bb40f2b3fd9
Ilya <zhilcka.ilya@yandex.ru>
2019-04-04 13:30:00 +0300

    Initial commit

--- src/main/java/com/luv2code/springdemo/mvc/CourseCode.java
+++ src/main/java/com/luv2code/springdemo/mvc/CourseCode.java
@@ -2,6 +2,6 @@ package com.luv2code.springdemo.mvc;
 
 public class CourseCode {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("hello world");
     }
 }
